ab08f257a649c0bd735b3a6ab57149f74e34a968
Ernesto Cruz Olivera <ernesto@ecruzolivera.tech>
2019-05-01 17:54:09 -0600

    material-ui-rating library added

--- src/components/PlaceCard.js
+++ src/components/PlaceCard.js
@@ -1,25 +1,35 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Card from "@material-ui/core/Card"
-import CardMedia from "@material-ui/core/CardMedia"
-import CardContent from "@material-ui/core/CardContent"
-import Typography from "@material-ui/core/Typography"
-import CardActions from "@material-ui/core/CardActions"
-import Button from "@material-ui/core/Button"
-import { withStyles } from "@material-ui/core/styles"
+import React from 'react'
+import PropTypes from 'prop-types'
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+import { withStyles } from '@material-ui/core/styles'
+import PhoneIcon from '@material-ui/icons/Phone'
+import EmailIcon from '@material-ui/icons/Email'
+import ExitToApp from '@material-ui/icons/ExitToApp'
+import Rating from 'material-ui-rating'
 
 const styles = theme => ({
   card: {
-    maxWidth: "600px",
-    display: "flex",
-    alignItems: "center"
+    maxWidth: '600px',
+    display: 'flex',
+    alignItems: 'center',
   },
   cardMedia: {
-    width: "200px",
-    height: "200px",
-    borderRadius: "4px",
-    marginLeft: "10px"
-  }
+    width: '200px',
+    height: '200px',
+    borderRadius: '4px',
+    marginLeft: theme.spacing.unit,
+  },
+  contact: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: theme.spacing.unit,
+  },
 })
 
 const PlaceCard = ({ name, contact, address, rating, img, classes }) => (
@@ -30,28 +40,53 @@ const PlaceCard = ({ name, contact, address, rating, img, classes }) => (
       image={img}
       title={`${name}`}
     />
-    <div>
+    <CardContent>
       <CardContent>
         <Typography variant='h5'>{name}</Typography>
-        <Typography variant='caption'>{`rating: ${rating}/4`}</Typography>
+        <Rating
+          value={rating}
+          max={4}
+          readOnly={true}
+        />
       </CardContent>
-      <CardContent>
-        <Typography variant='body1' align='right'>
-          {address.street}
-        </Typography>
-        <Typography variant='body1' align='right'>
-          {address.city}
-        </Typography>
-        <Typography variant='body1' align='right'>
-          {address.state}
-        </Typography>
-      </CardContent>
-      <CardContent>
-        <Typography variant='body1'>{`Tel: ${contact.phone}`}</Typography>
-        <Typography variant='body1'>{`email: ${contact.email}`}</Typography>
-        <Typography variant='body1'>{contact.site}</Typography>
-      </CardContent>
-    </div>
+      <Typography variant='body1' align='right'>
+        {address.street}
+      </Typography>
+      <Typography variant='body1' align='right'>
+        {address.city}
+      </Typography>
+      <Typography variant='body1' align='right'>
+        {address.state}
+      </Typography>
+      <div className={classes.contact}>
+        <PhoneIcon className={classes.icon} />
+        <Typography variant='body1'>{contact.phone}</Typography>
+      </div>
+      <div className={classes.contact}>
+        <EmailIcon className={classes.icon} />
+        <Link
+          href={`mailto:${contact.email.toLowerCase()}`}
+          target='_blank'
+          rel='noreferrer'
+          variant='body1'
+          color='textPrimary'
+        >
+          {contact.email.toLowerCase()}
+        </Link>
+      </div>
+      <div className={classes.contact}>
+        <ExitToApp className={classes.icon} />
+        <Link
+          href={contact.site.toLowerCase()}
+          target='_blank'
+          rel='noreferrer'
+          variant='body1'
+          color='textPrimary'
+        >
+          {contact.site}
+        </Link>
+      </div>
+    </CardContent>
   </Card>
 )
 
@@ -60,7 +95,8 @@ PlaceCard.propTypes = {
   contact: PropTypes.object.isRequired,
   address: PropTypes.object.isRequired,
   rating: PropTypes.number.isRequired,
-  classes: PropTypes.object.isRequired
+  img: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
 export default withStyles(styles)(PlaceCard)
--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -1,19 +1,16 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import InputBase from '@material-ui/core/InputBase'
-import { fade } from '@material-ui/core/styles/colorManipulator'
-import { withStyles } from '@material-ui/core/styles'
-import MenuIcon from '@material-ui/icons/Menu'
-import SearchIcon from '@material-ui/icons/Search'
-import CloseIcon from '@material-ui/icons/Close'
+import React from "react"
+import PropTypes from "prop-types"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import InputBase from "@material-ui/core/InputBase"
+import { fade } from "@material-ui/core/styles/colorManipulator"
+import { withStyles } from "@material-ui/core/styles"
+import SearchIcon from "@material-ui/icons/Search"
 
 const styles = theme => ({
   root: {
-    width: '100%'
+    width: "100%"
   },
   grow: {
     flexGrow: 1
@@ -23,49 +20,45 @@ const styles = theme => ({
     marginRight: 20
   },
   title: {
-    // display: 'none',
-    // [theme.breakpoints.up('sm')]: {
-    //   display: 'block'
-    // }
-    marginRight: '1rem'
+    marginRight: "1rem"
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
+    "&:hover": {
       backgroundColor: fade(theme.palette.common.white, 0.25)
     },
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing.unit,
-      width: 'auto'
+      width: "auto"
     }
   },
   searchIcon: {
     width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   },
   inputRoot: {
-    color: 'inherit',
-    width: '100%'
+    color: "inherit",
+    width: "100%"
   },
   inputInput: {
     paddingTop: theme.spacing.unit,
     paddingRight: theme.spacing.unit,
     paddingBottom: theme.spacing.unit,
     paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       width: 120,
-      '&:focus': {
+      "&:focus": {
         width: 200
       }
     }
@@ -86,12 +79,7 @@ const TopAppBar = props => {
           >
             <MenuIcon />
           </IconButton> */}
-          <Typography
-            className={classes.title}
-            variant='h6'
-            color='inherit'
-            // noWrap
-          >
+          <Typography className={classes.title} variant='h6' color='inherit'>
             Melp
           </Typography>
           <div className={classes.grow} />
