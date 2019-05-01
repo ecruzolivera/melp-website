ba3a72966e47d5c4ebaafe8b7f084d1756cc868f
Ernesto Cruz Olivera <ernesto@ecruzolivera.tech>
2019-05-01 14:10:18 -0600

    Updates

---
+++ precache-manifest.7c53f308703d042736826aa85c3b7ddf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9b0722b4211497a491cb4b0647ca73",
+    "url": "/melp-website/index.html"
+  },
+  {
+    "revision": "c4b61147538ad85ca738",
+    "url": "/melp-website/static/js/2.fc007088.chunk.js"
+  },
+  {
+    "revision": "9773d498755c3e0e61e5",
+    "url": "/melp-website/static/js/main.712defe2.chunk.js"
+  },
+  {
+    "revision": "9cdf3364c52a4759a77d",
+    "url": "/melp-website/static/js/runtime~main.62ae8cb2.js"
+  }
+]);
