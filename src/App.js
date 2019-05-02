b2fe8153a7bb87fa468d32b21bdc4dcd9457fcdc
Ernesto Cruz Olivera <ernesto@ecruzolivera.tech>
2019-05-02 02:35:33 -0600

    BottomNav Component Added Minor layouts fixes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import TopAppBar from './components/TopAppBar'
 import PlaceCard from './components/PlaceCard'
+import BottomNav from './components/BottomNav'
 import { withStyles } from '@material-ui/core/styles'
 import DataFetcher from './services/DataFetcher'
 
 const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    minHeight: '100vh',
+  },
   placeList: {
     display: 'flex',
     flexDirection: 'column',
     alignContent: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    flexGrow: 1,
   },
   placeItem: {
     margin: theme.spacing.unit,
@@ -28,12 +36,11 @@ class App extends Component {
   async componentDidMount() {
     let data = []
     try {
-      data = await DataFetcher(2)
+      data = await DataFetcher(1)
     } catch (error) {
       console.error(error)
     }
     this.setState({ data: data })
-    console.log(data)
   }
   render() {
     const { classes } = this.props
@@ -41,22 +48,29 @@ class App extends Component {
     return (
       <Fragment>
         <CssBaseline />
-        <TopAppBar onChange={console.log} />
-        <div className={classes.placeList}>
-          {data && data.length > 0 ? (
-            data.map(item => (
-              <div key={item.id} className={classes.placeItem}>
-                <PlaceCard
-                  {...item}
-                  img={`http://lorempixel.com/200/200/food/${Math.floor(
-                    Math.random() * data.length,
-                  ) + 1}`}
-                />
-              </div>
-            ))
-          ) : (
-            <CircularProgress className={classes.placeItem} />
-          )}
+        <div className={classes.root}>
+          <TopAppBar onChange={console.log} />
+          <div className={classes.placeList}>
+            {data && data.length > 0 ? (
+              data.map(item => (
+                <div key={item.id} className={classes.placeItem}>
+                  <PlaceCard
+                    {...item}
+                    img={`http://lorempixel.com/200/200/food/${Math.floor(
+                      Math.random() * data.length,
+                    ) + 1}`}
+                  />
+                </div>
+              ))
+            ) : (
+              <CircularProgress className={classes.placeItem} />
+            )}
+          </div>
+          <BottomNav
+            currentValue={1}
+            maxValue={5}
+            onChange={(event, value) => console.log(value)}
+          />
         </div>
       </Fragment>
     )
---
+++ src/components/BottomNav.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft'
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+const BottomNav = ({ currentValue, maxValue, onChange, classes }) => (
+  <BottomNavigation onChange={onChange} className={classes.root}>
+    <BottomNavigationAction
+      label='Previous'
+      value='Previous'
+      icon={<KeyboardArrowLeft />}
+    />
+    <Typography variant='body1' align='right'>
+      {`${currentValue} - ${maxValue}`}
+    </Typography>
+    <BottomNavigationAction
+      label='Next'
+      value='Next'
+      icon={<KeyboardArrowRight />}
+    />
+  </BottomNavigation>
+)
+
+BottomNav.propTypes = {
+  currentValue: PropTypes.number.isRequired,
+  maxValue: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(BottomNav)
