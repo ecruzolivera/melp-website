56f647049dcecc93717e08c199d569a4d8da8375
Ernesto Cruz Olivera <ernesto@ecruzolivera.tech>
2019-05-01 23:15:37 -0600

    lorempixel website random is not THAT random So I force a random number from 1 to the lenght of the data.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,78 @@
-import React, { Component, Fragment } from "react"
-import Grid from "@material-ui/core/Grid"
-import CircularProgress from "@material-ui/core/CircularProgress"
-import CssBaseline from "@material-ui/core/CssBaseline"
+import React, { Component, Fragment } from 'react'
+import Grid from '@material-ui/core/Grid'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-import TopAppBar from "./components/TopAppBar"
-import PlaceCard from "./components/PlaceCard"
+import TopAppBar from './components/TopAppBar'
+import PlaceCard from './components/PlaceCard'
 
-import { withStyles } from "@material-ui/core/styles"
+import { withStyles } from '@material-ui/core/styles'
 
 const data = [
   {
-    id: "851f799f-0852-439e-b9b2-df92c43e7672",
+    id: '851f799f-0852-439e-b9b2-df92c43e7672',
     rating: 1,
-    name: "Barajas, Bahena and Kano",
+    name: 'Barajas, Bahena and Kano',
     contact: {
-      site: "https://federico.com",
-      email: "Anita_Mata71@hotmail.com",
-      phone: "534 814 204"
+      site: 'https://federico.com',
+      email: 'Anita_Mata71@hotmail.com',
+      phone: '534 814 204',
     },
     address: {
-      street: "82247 Mariano Entrada",
-      city: "Mérida Alfredotown",
-      state: "Durango",
-      location: { lat: 19.440057053713137, lng: -99.12704709742486 }
-    }
-  }
+      street: '82247 Mariano Entrada',
+      city: 'Mérida Alfredotown',
+      state: 'Durango',
+      location: { lat: 19.440057053713137, lng: -99.12704709742486 },
+    },
+  },
+  {
+    id: '4e17896d-a26f-44ae-a8a4-5fbd5cde79b0',
+    rating: 0,
+    name: 'Hernández - Lira',
+    contact: {
+      site: 'http://graciela.com.mx',
+      email: 'Brandon_Vigil@hotmail.com',
+      phone: '570 746 998',
+    },
+    address: {
+      street: '93725 Erick Arroyo',
+      city: 'Mateofurt',
+      state: 'Hidalgo',
+      location: { lat: 19.437904276994995, lng: -99.12865767750226 },
+    },
+  },
+  {
+    id: 'c0ffd058-e773-47f1-974b-42d41cb555bf',
+    rating: 3,
+    name: 'Rendón - Elizondo',
+    contact: {
+      site: 'https://cristina.mx',
+      email: 'Hugo.Casanova49@gmail.com',
+      phone: '5866-337-812',
+    },
+    address: {
+      street: '5518 Monserrat Explanada',
+      city: 'Chignahuapan María',
+      state: 'Sinaloa',
+      location: { lat: 19.43607059103484, lng: -99.12978657319944 },
+    },
+  },
+  {
+    id: 'c29082c4-4352-4517-9b4b-c45f86fc1830',
+    rating: 2,
+    name: 'Nájera - Chávez',
+    contact: {
+      site: 'https://pedro.gob.mx',
+      email: 'Carlota31@hotmail.com',
+      phone: '5532-129-406',
+    },
+    address: {
+      street: '79224 Mariano Travesía',
+      city: 'Nezahualcóyotl Timoteo',
+      state: 'Coahuila',
+      location: { lat: 19.442486911665654, lng: -99.12383325991955 },
+    },
+  },
 ]
 
 function App() {
@@ -39,9 +87,14 @@ function App() {
         alignItems='center'
       >
         {data ? (
-          <Grid key={data[0].id} item>
-            <PlaceCard {...data[0]} img='http://lorempixel.com/200/200/food/' />
-          </Grid>
+          data.map(item => (
+            <Grid key={item.id} item>
+              <PlaceCard
+                {...item}
+                img={`http://lorempixel.com/200/200/food/${Math.floor(Math.random()*data.length)+1}`}
+              />
+            </Grid>
+          ))
         ) : (
           <CircularProgress />
         )}
